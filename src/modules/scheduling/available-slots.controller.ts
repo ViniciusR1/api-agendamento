16e73878dd8620f86f2fc14c03aedc351bc5351e
Vinicius Rodrigues <viniciuscavalcanter1@gmail.com>
2026-08-12 18:27:17 -0300

    fix: corrige regex, para funcionar o date

--- src/modules/scheduling/available-slots.controller.ts
+++ src/modules/scheduling/available-slots.controller.ts
@@ -18,7 +18,7 @@ export class AvailableSlotsController {
     if (typeof serviceId !== "string" || typeof date !== "string") {
       throw new BadRequestError("serviceId and date query params are required");
     }
-    if(!/^\d{4}-\d{2}$/.test(date)) {
+    if(!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
       throw new BadRequestError('Data deve estar no formato AAAA-MM-DD')
     }
     const parsedDate = new Date(`${date}T00:00:00.000Z`);
